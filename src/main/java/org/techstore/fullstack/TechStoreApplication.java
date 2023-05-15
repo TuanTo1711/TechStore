9a5ee5b2489e9ce207ba19daf6b116477621b6d1
Yuuta <totunvtc2003@gmail.com>
2023-05-15 22:31:24 +0700

    Init project structure

---
+++ src/main/java/org/techstore/fullstack/TechStoreApplication.java
@@ -0,0 +1,13 @@
+package org.techstore.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechStoreApplication.class, args);
+    }
+
+}
